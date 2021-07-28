b032c8069b28ed06b2a000e5fb46f9d71d6f737a
yeon <yeoncheol.jang@gmail.com>
2021-07-28 13:26:22 +0900

    [Changed] referenced table name

--- src/main/java/com/example/springboot/entity/Reply.java
+++ src/main/java/com/example/springboot/entity/Reply.java
@@ -13,7 +13,7 @@ import java.io.Serializable;
 @NoArgsConstructor @AllArgsConstructor
 @Builder
 @Entity
-@Table(name = "Reply")
+@Table(name = "reply")
 public class Reply implements Serializable {
     private static final long serialVersionUID = 1L;
 
