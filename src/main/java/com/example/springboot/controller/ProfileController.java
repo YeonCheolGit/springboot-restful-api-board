2226afd6dcbc9accc5cbbaa046a4492edf42ba1d
yeon <yeoncheol.jang@gmail.com>
2021-08-01 01:19:23 +0900

    [Added] check profile controller for deployment

---
+++ src/main/java/com/example/springboot/controller/ProfileController.java
@@ -0,0 +1,26 @@
+package com.example.springboot.controller;
+
+import io.swagger.annotations.Api;
+import lombok.RequiredArgsConstructor;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/profile")
+@Api(tags = {"6. Board"})
+public class ProfileController {
+
+    private final Environment environment;
+
+    @GetMapping
+    public String profile() {
+        return Arrays.stream(environment.getActiveProfiles())
+                .findFirst()
+                .orElse("");
+    }
+}
